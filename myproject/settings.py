5315ada86d05ae89d7924d98c712d2bbe4d7db7a
Maaack <marek.belski@gmail.com>
2017-03-15 15:13:40 -0700

    Update location of local_settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -307,11 +307,11 @@ OPTIONAL_APPS = (
 # local_settings has full access to everything defined in this module.
 # Also force into sys.modules so it's visible to Django's autoreload.
 
-f = os.path.join(PROJECT_APP_PATH, "local_settings.py")
+f = os.path.join(PROJECT_ROOT, "local_settings.py")
 if os.path.exists(f):
     import sys
     import imp
-    module_name = "%s.local_settings" % PROJECT_APP
+    module_name = "%s.local_settings" % PROJECT_ROOT
     module = imp.new_module(module_name)
     module.__file__ = f
     sys.modules[module_name] = module
